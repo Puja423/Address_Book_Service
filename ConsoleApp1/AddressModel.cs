dc9d642dfe5155efdd5bfc15676d0d0b9c75a27a
Puja <pujayadav@gmail.com>
2020-11-10 21:49:28 +0530

    [puja] add . delete contact details

--- ConsoleApp1/AddressModel.cs
+++ ConsoleApp1/AddressModel.cs
@@ -1,10 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace ConsoleApp1
 {
     class AddressModel
     {
+        DataTable dataTable = new DataTable();
+
+        public void CreateAddressBook()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(int));
+            dataTable.Columns.Add("Email", typeof(string));
+        }
+        public void ContactDetails()
+        {
+            dataTable.Rows.Add("Toshani", "Yadav", "Lotus", "Noida", "UP", "201310", "12345687","toshi@hmail.com");
+            dataTable.Rows.Add("Kaminee", "Uikey", "abcd", "Seoni", "MP", "455668", "154256", "ksmiskj@gmail.com");
+            dataTable.Rows.Add("Rakhi", "saraf", "qwert", "Sitamarhi", "Bihar", "26273898", "2782863", "ksjjklaL@gmail.com");
+            dataTable.Rows.Add("Sameer", "Chaudhary", "ahdff", "Bhopal", "MP", "6254443332", "3897487", "sameerfaltu@gmail.com");
+        }
+        public void DisplayAddressBook()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn col in dataTable.Columns)
+                {
+                    Console.Write(row[col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// Edits the existing contact in datatable
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="zipcode"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="email"></param>
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipcode, string phoneNumber, string email)
+        {
+            var recordedData = dataTable.AsEnumerable().Where(x => x.Field<string>("FirstName") == firstName).FirstOrDefault();
+            if (recordedData != null)
+            {
+                recordedData.SetField("LastName", lastName);
+                recordedData.SetField("Address", address);
+                recordedData.SetField("City", city);
+                recordedData.SetField("State", state);
+                recordedData.SetField("ZipCode", zipcode);
+                recordedData.SetField("EmailID", email);
+                recordedData.SetField("State", state);
+                Console.WriteLine("Contact edited successfully");
+            }
+            else
+            {
+                Console.WriteLine("No Contact Found");
+            }
+        }
+        public void DeleteContact(string name)
+        {
+            var deleteRow = dataTable.AsEnumerable().Where(a => a.Field<string>("FirstName").Equals(name)).FirstOrDefault();
+            if (deleteRow != null)
+            {
+                deleteRow.Delete();
+                Console.WriteLine("Contact deleted successfully");
+            }
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace AddressBookLinq
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To  AddressBook Service");
+            Console.WriteLine("Welcome to address book system");
+            Addressbook book = new AddressBookLinq();
+            int loop = 1;
+            book.CreateAddressBook();
+            while (loop == 1)
+            {
+                Console.WriteLine("Make choice according to your desired operation \n1. Display Address book \n2. Insert Contact \n3. Edit Contact \n4. Delete Contact \n5. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        book.DisplayAddressBook();
+                        break;
+                    case 2:
+                        Contact contact = new Contact();
+                        Console.WriteLine("Enter the person details to be added in the address book");
+                        Console.WriteLine("First Name");
+                        contact.FirstName = Console.ReadLine();
+                        Console.WriteLine("Last Name");
+                        contact.LastName = Console.ReadLine();
+                        Console.WriteLine("Address");
+                        contact.Address = Console.ReadLine();
+                        Console.WriteLine("City");
+                        contact.City = Console.ReadLine();
+                        Console.WriteLine("State");
+                        contact.State = Console.ReadLine();
+                        Console.WriteLine("Zip code");
+                        contact.ZipCode = Console.ReadLine();
+                        Console.WriteLine("Phone Number");
+                        contact.PhoneNumber = Console.ReadLine();
+                        Console.WriteLine("Email");
+                        contact.Email = Console.ReadLine();
+                        book.InsertContacts(contact);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the person details to be added in the address book");
+                        Console.WriteLine("First Name");
+                        string FirstName = Console.ReadLine();
+                        Console.WriteLine("Last Name");
+                        string LastName = Console.ReadLine();
+                        Console.WriteLine("Address");
+                        string Address = Console.ReadLine();
+                        Console.WriteLine("City");
+                        string City = Console.ReadLine();
+                        Console.WriteLine("State");
+                        string State = Console.ReadLine();
+                        Console.WriteLine("Zip code");
+                        string ZipCode = Console.ReadLine();
+                        Console.WriteLine("Phone Number");
+                        string PhoneNumber = Console.ReadLine();
+                        Console.WriteLine("Email");
+                        string Email = Console.ReadLine();
+                        book.EditContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter FirstName of contact to be deleted");
+                        string name = Console.ReadLine();
+                        book.DeleteContact(name);
+                        break;
+                    case 5:
+                        loop = 0;
+                        break;
+                }
+            }
         }
     }
 }
